f370052e9d030690338fd2cdeb0e4e914e90bd17
Dana <budadaniela68@yahoo.com>
2020-11-10 15:48:29 +0200

    Mici mari update-uri
    - Facut layer nou pentru propsurile cu collision
    - Pus cateva propsuri ca children la propsurile principale (mere la pom, sabii si foc la castronul de foc etc.)
    - Facut camera sa se miste dupa caracter + limite sa nu iasa din harta
    - Modificat mapa putin cu tile-urile corespunzatoare.
    - Adaugat un script provizoriu de movement la caracter.

--- KeepJumping/Assets/Scripts/CameraFollow.cs
+++ KeepJumping/Assets/Scripts/CameraFollow.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraFollow1 : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    private Transform playerTransform;
+    [SerializeField]
+    private float leftLimit;
+    [SerializeField]
+    private float topLimit;
+    [SerializeField]
+    private float rightLimit;
+    [SerializeField]
+    private float bottomLimit;
+    
     void Start()
     {
-        print("test")
+        playerTransform = GameObject.FindGameObjectWithTag ("Player").transform;
     }
-
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
+        Vector3 temp = transform.position; // iau pozitia camerei
+
+        temp.x = playerTransform.position.x; // pun pozitia caracterului la camera
+        temp.y = playerTransform.position.y;
         
+        transform.position = temp; // updatez pozitia camerei
+        
+        // stript pentru limitarea camerei
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y,bottomLimit,topLimit),
+            transform.position.z
+        );
+
     }
 }
